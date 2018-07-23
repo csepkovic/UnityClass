c6d32bda1073365024493b7a2b0bcc66891deb97
Colin Sepkovic <uberdude858@gmail.com>
2018-07-23 13:43:13 -0700

    Day 1

---
+++ Runner/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour {
+    public Rigidbody2D rigidBody;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            rigidBody.AddForce(new Vector2(0, 500));
+        }
+	}
+}
